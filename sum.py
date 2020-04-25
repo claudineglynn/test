8ca9a84e7833d527637c46bb7b1d2ec6df65de6e
claudine glynn <jjc3po@gmail.com>
2020-04-25 17:15:52 +0000

    test

---
+++ sum.py
@@ -0,0 +1,29 @@
+s = "hello how are you"
+i = 100
+b = True
+d = {"my name":"darren", "my age":45}
+a = [100, 20 ,50, 68]
+
+
+firstValue = int(input("first number >>"))
+secondValue = int(input("second number >>"))
+
+'''
+j = 0
+for i in a:
+    print(i)
+    j = j + i
+print("the sum is", j)
+
+sumofall = sum(a)
+print(sumofall)
+
+'''
+
+print(firstValue + secondValue)
+
+if condition:
+    pass
+else:
+    pass
+
---
+++ test.py
@@ -0,0 +1,14 @@
+# this is a file
+
+
+a = 100
+b = 20
+
+print(a+b)
+
+
+for i in range(20):
+    print("hello " + str(i))
+
+
+
